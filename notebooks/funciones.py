cebf6dfd7a3eda7f38bf720a723ece763cf09a5b
jorgefreires <jorge.freire.serrano@gmail.com>
2023-03-13 12:57:44 +0100

    cambios_limpieza

--- notebooks/funciones.py
+++ notebooks/funciones.py
@@ -2,55 +2,45 @@ import warnings
 import pyarrow
 import pandas as pd
 from tqdm import tqdm
-import funciones as f
 
 def limpieza(lista):
 
-    try:
+    dfs = []
 
-        dfs = []
+    for e in tqdm(lista):
 
-        for e in tqdm(lista):
+        try: 
 
-            # Se importa el archivo
+             # Se importa el archivo
 
-            df = pd.read_csv(f'../../../../../../../../Volumes/JFreire/Databases/trafic_mad/{e}', sep=';')
+            df = pd.read_csv(f'../../../../../../Documents/Databases/trafic_mad/{e}', sep=';', encoding_errors='ignore')
 
             # Realmente solo me interesan las columnas fecha, y intensidad. Se borra el resto
 
-            try: 
-                
-                df = df[['fecha', 'intensidad']]
+            df = df[['fecha', 'intensidad']]
 
-                # Lo que se quiere es tener la media de intensidad del tráifco en toda la ciudad
+            # Lo que se quiere es tener la media de intensidad del tráifco en toda la ciudad
 
-                df = df.groupby(['fecha']).mean()
+            df = df.groupby(['fecha']).mean()
 
-                df.reset_index(inplace=True)
+            df.reset_index(inplace=True)
 
-                # Se añade el df a la lista para su posterior concatenación
+            # Se añade el df a la lista para su posterior concatenación
 
-                dfs.append(df)
+            dfs.append(df)
+        
+        except: 
+
+            # Hay un csv con el separador ',' en vez de ';'
             
-            except: 
+            df = pd.read_csv(f'../../../../../../Documents/Databases/trafic_mad/{e}', sep=',', encoding_errors='ignore')
+            
+            df = df[['fecha', 'intensidad']]
 
-                # Hay un csv con el separador ',' en vez de ';'
-                
-                df = pd.read_csv(f'../../../../../../../../Volumes/JFreire/Databases/trafic_mad/{e}', sep=',')
-                
-                df = df[['fecha', 'intensidad']]
+            df = df.groupby(['fecha']).mean()
 
-                df = df.groupby(['fecha']).mean()
+            df.reset_index(inplace=True)
 
-                df.reset_index(inplace=True)
-
-                dfs.append(df)
-
-
-    except:
-
-        warnings.warn('No se ha podido importar el archivo')
-
-        print(e)
+            dfs.append(df)
 
     return dfs
